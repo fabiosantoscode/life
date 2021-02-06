341464673d52e478f38ce382fde1c415fae1b4dd
Fábio Santos <fabiosantosart@gmail.com>
2021-02-06 16:14:25 +0000

    update things

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
 extern crate piston_window;
 
-use std::option::Option;
 use piston_window::*;
+use std::option::Option;
 
 fn create_window() -> PistonWindow {
     return WindowSettings::new("Life", [640, 480])
@@ -12,7 +11,7 @@ fn create_window() -> PistonWindow {
 }
 
 fn create_map() -> Vec<bool> {
-    let mut ret = vec!(false; 64 * 48);
+    let mut ret = vec![false; 64 * 48];
 
     //  x
     //   x
@@ -44,18 +43,20 @@ fn is_cell_live(map: &mut Vec<bool>, x: usize, y: usize) -> bool {
 }
 
 // added 64 and 48 respectively to avoid negative numbers when using modulo
-const POSSIBLE_NEIGHBOURS_PAIRS: [[usize; 2]; 8] = [// top row
-                                                    [64 - 1, 48 - 1],
-                                                    [64 - 1, 48 + 0],
-                                                    [64 - 1, 48 + 1],
-                                                    // middle row (only two because 0,0 is the cell
-                                                    // itself, not a neighbour
-                                                    [64 + 0, 48 - 1],
-                                                    [64 + 0, 48 + 1],
-                                                    // bottom row
-                                                    [64 + 1, 48 - 1],
-                                                    [64 + 1, 48 + 0],
-                                                    [64 + 1, 48 + 1]];
+const POSSIBLE_NEIGHBOURS_PAIRS: [[usize; 2]; 8] = [
+    // top row
+    [64 - 1, 48 - 1],
+    [64 - 1, 48 + 0],
+    [64 - 1, 48 + 1],
+    // middle row (only two because 0,0 is the cell
+    // itself, not a neighbour
+    [64 + 0, 48 - 1],
+    [64 + 0, 48 + 1],
+    // bottom row
+    [64 + 1, 48 - 1],
+    [64 + 1, 48 + 0],
+    [64 + 1, 48 + 1],
+];
 
 fn does_cell_live(map: &mut Vec<bool>, x: usize, y: usize) -> bool {
     let mut live_neighbour_count = 0;
@@ -67,10 +68,10 @@ fn does_cell_live(map: &mut Vec<bool>, x: usize, y: usize) -> bool {
     }
     if is_cell_live(map, x, y) {
         if live_neighbour_count < 2 {
-            return false;  // starvation
+            return false; // starvation
         }
         if live_neighbour_count > 3 {
-            return false;  // overpopulation
+            return false; // overpopulation
         }
         return true;
     } else {
@@ -82,7 +83,7 @@ fn does_cell_live(map: &mut Vec<bool>, x: usize, y: usize) -> bool {
 }
 
 fn update_life(map: &mut Vec<bool>) {
-    let mut cpy = vec![true;64 * 48];
+    let mut cpy = vec![true; 64 * 48];
     cpy.copy_from_slice(map);
     for x in 0..64 {
         for y in 0..48 {
@@ -97,19 +98,23 @@ const COLOR_ON: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 fn draw_life(map: &mut Vec<bool>, state: AppState, c: Context, g: &mut G2d) {
     for x in 0..64 {
         let screen_x = x as f64;
-        line(COLOR_LINE,
-             0.5,
-             [screen_x * 10.0, 0.0, screen_x * 10.0, 480.0],
-             c.transform,
-             g);
+        line(
+            COLOR_LINE,
+            0.5,
+            [screen_x * 10.0, 0.0, screen_x * 10.0, 480.0],
+            c.transform,
+            g,
+        );
     }
     for y in 0..48 {
         let screen_y = y as f64;
-        line(COLOR_LINE,
-             0.5,
-             [0.0, screen_y * 10.0, 640.0, screen_y * 10.0],
-             c.transform,
-             g);
+        line(
+            COLOR_LINE,
+            0.5,
+            [0.0, screen_y * 10.0, 640.0, screen_y * 10.0],
+            c.transform,
+            g,
+        );
     }
     for x in 0..64 {
         for y in 0..48 {
@@ -117,10 +122,12 @@ fn draw_life(map: &mut Vec<bool>, state: AppState, c: Context, g: &mut G2d) {
             let screen_y = y as f64;
             let on = map[(y * 64) + x];
             if on {
-                rectangle(COLOR_ON,
-                          [screen_x * 10.0, screen_y * 10.0, 9.0, 9.0],
-                          c.transform,
-                          g);
+                rectangle(
+                    COLOR_ON,
+                    [screen_x * 10.0, screen_y * 10.0, 9.0, 9.0],
+                    c.transform,
+                    g,
+                );
             }
         }
     }
@@ -130,24 +137,22 @@ fn draw_life(map: &mut Vec<bool>, state: AppState, c: Context, g: &mut G2d) {
         } else {
             [1.0, 0.1, 0.1, 1.0]
         };
-        rectangle(rect_color,
-                  [(state.x as f64) * 10.0, (state.y as f64) * 10.0, 9.0, 9.0],
-                  c.transform,
-                  g);
+        rectangle(
+            rect_color,
+            [(state.x as f64) * 10.0, (state.y as f64) * 10.0, 9.0, 9.0],
+            c.transform,
+            g,
+        );
     }
 }
 
-#[derive(PartialEq)]
-#[derive(Clone)]
-#[derive(Copy)]
+#[derive(PartialEq, Clone, Copy)]
 enum AppMode {
     Normal,
     Editing,
 }
 
-#[derive(PartialEq)]
-#[derive(Clone)]
-#[derive(Copy)]
+#[derive(PartialEq, Clone, Copy)]
 struct AppState {
     x: usize,
     y: usize,
@@ -178,38 +183,47 @@ fn toggle_app_state(state: AppState) -> AppState {
 
 fn update_state_from_input(inp: Input, state: AppState) -> Option<AppState> {
     return match inp {
-        Input::Press(Button::Keyboard(Key::Space)) => Some(toggle_app_state(state)),
-        Input::Move(Motion::MouseCursor(mouse_x, mouse_y)) => {
-            Some(AppState {
-                x: (mouse_x / 10.0).floor() as usize % 64,
-                y: (mouse_y / 10.0).floor() as usize % 48,
-                down: state.down,
-                mode: state.mode,
-            })
-        }
-        Input::Press(Button::Mouse(MouseButton::Left)) => {
-            Some(AppState {
-                x: state.x,
-                y: state.y,
-                down: true,
-                mode: state.mode,
-            })
-        }
-        Input::Release(Button::Mouse(MouseButton::Left)) => {
-            Some(AppState {
-                x: state.x,
-                y: state.y,
-                down: false,
-                mode: state.mode,
-            })
-        }
+        Input::Button(ButtonArgs {
+            button: Button::Keyboard(Key::Space),
+            state: ButtonState::Press,
+            ..
+        }) => Some(toggle_app_state(state)),
+        Input::Move(Motion::MouseCursor([mouse_x, mouse_y])) => Some(AppState {
+            x: (mouse_x / 10.0).floor() as usize % 64,
+            y: (mouse_y / 10.0).floor() as usize % 48,
+            down: state.down,
+            mode: state.mode,
+        }),
+        Input::Button(ButtonArgs {
+            button: Button::Mouse(MouseButton::Left),
+            state: ButtonState::Press,
+            ..
+        }) => Some(AppState {
+            x: state.x,
+            y: state.y,
+            down: true,
+            mode: state.mode,
+        }),
+        Input::Button(ButtonArgs {
+            button: Button::Mouse(MouseButton::Left),
+            state: ButtonState::Release,
+            ..
+        }) => Some(AppState {
+            x: state.x,
+            y: state.y,
+            down: false,
+            mode: state.mode,
+        }),
         _ => None,
     };
 }
 
+// Don't go too fast
+const FPS: u64 = 8;
+
 fn main() {
     let mut window: PistonWindow = create_window();
-    window.set_max_fps(8);
+    window.set_max_fps(FPS);
     let mut map = create_map();
     let mut state = AppState {
         x: 0,
@@ -218,7 +232,7 @@ fn main() {
         mode: AppMode::Normal,
     };
     while let Some(e) = window.next() {
-        if let Event::Input(e) = e {
+        if let Event::Input(e, _) = e {
             if let Some(new_state) = update_state_from_input(e, state) {
                 if let AppMode::Editing = state.mode {
                     edit_life(state, &mut map);
@@ -226,7 +240,7 @@ fn main() {
                 state = new_state;
             }
         } else {
-            window.draw_2d(&e, |c, mut g| {
+            window.draw_2d(&e, |c, mut g, _| {
                 clear([1.0; 4], g);
                 draw_life(&mut map, state, c, &mut g);
                 if let AppMode::Normal = state.mode {
